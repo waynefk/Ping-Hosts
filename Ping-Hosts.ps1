cd0da0c548b1e15903867b59481184622943d062
BitterVagabond <44381248+waynefk@users.noreply.github.com>
2018-11-25 18:07:17 -0800

    Add experimental section
    I've added a commented section to experiment with taking in many hosts names and dividing them equally into multiple files.

--- Ping-Hosts.ps1
+++ Ping-Hosts.ps1
@@ -1,3 +1,25 @@
+<# This commented section is an unfinished experiment with logic on pulling in many names and breaking them up into smaller sections
+$sourcefilename = "C:\Users\wayne\Documents\Scripts\filenames.txt"
+
+$maxsize = 10
+
+$filenumber = 1
+
+$destfolder = "C:\Users\wayne\Documents\Scripts"
+
+$linecount = 0
+
+$content = get-content $sourcefilename | % { 
+ Add-Content $destfolder\hosts$filenumber.txt "$_" 
+  $linecount ++ 
+  If ($linecount -eq $maxsize) { 
+    $filenumber++ 
+    $linecount = 0 
+  } 
+} 
+#>
+
+
 workflow Ping-Hosts
     {
         param
